7ebdbd267ec98e210e294bd4ef58048bc1ef9d03
Ravi <samcollins9899@gmail.com>
2025-12-04 00:17:08 +0530

    feat: updated prefrences api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,5 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/users/signup', registerUser);
-router.post('/users/login', loginUser);
 
 module.exports = router;
